f2694d0031f9539d16815f13de9c8070ade47f70
shwetraval <ravalshwet25@gmail.com>
2025-01-21 17:14:53 +0530

    Add files via upload

---
+++ Numericalfunction.php
@@ -0,0 +1,12 @@
+<?php
+echo "absolute value = ".abs (-9)."<br>";
+echo "celi value = ".ceil (4.8)."<br>";
+echo "floor value = ".floor(5.3)."<br>";
+echo "round value = ".round(4.9, 1)."<br>"; 
+echo "max value = ".max(1, 6, 3, 9, 5)."<br>"; 
+echo "min value = ".min(1, 6, 3, 9, 5)."<br>"; 
+echo "power value = ".pow(2, 8)."<br>"; 
+echo "random value = ".rand(1, 9)."<br>";
+echo "squre root value = ".sqrt(26)."<br>";
+echo "fmod value = ".fmod(7, 5)."<br>"; 
+?>
