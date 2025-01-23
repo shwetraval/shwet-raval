df745474b0d31fd2e23ee6d8061e2a5a507e5a70
shwetraval <ravalshwet25@gmail.com>
2025-01-23 22:02:31 +0530

    Add files via upload

---
+++ static keyword.java
@@ -0,0 +1,26 @@
+class static keyword
+{ static keyword
+	int rollno;
+	String name;
+	static keyword
+	static keyword(int number,String b)
+		{
+          rollno=number;
+          name=b;
+		}
+	statickeyword String college="cusp";
+		void display()
+		{
+			System.out.println("rollno ="+rollno+"name ="+name+"college ="+college);
+		}
+
+	public static void main(String args[])
+
+	{
+     pr9 s1=new pr9(123,"nitesh");
+     pr9 s2=new pr9(7125,"nilesh");
+     s1.display();
+     s2.display();
+
+	}
+}
